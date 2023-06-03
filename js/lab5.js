2987650170e16a71cf2115d3a23771f64b04652b
Tsogt Enkhbat <kikoislife18@gmail.com>
2023-06-02 23:24:51 -0500

    Update lab5.js

--- js/lab5.js
+++ js/lab5.js
@@ -1,9 +1,9 @@
-const exerciseOne = function (arr) {
-  return arr.filter((x) => x > 20).reduce((a, b) => a + b, 0);
+const exerciseOne = function (array) {
+  return array.filter((x) => x > 20).reduce((a, b) => a + b, 0);
 }
 
-const exerciseTwo = function (arr) {
-  return arr
+const exerciseTwo = function (array) {
+  return array
     .filter((e) => e.length >= 5 && e.includes("a"))
     .map((e) => e);
 };
